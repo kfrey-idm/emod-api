973e4f7745e4c0af91a52725563a06ebd5453eb3
Kurt Frey <kfrey@idmod.org>
2026-02-12 10:11:25 -0800

    Align workflows (#75)

--- emod_api/__init__.py
+++ emod_api/__init__.py
@@ -1,13 +1,1 @@
-"""
-To generate a config.json from a param_overrides.json (or params-of-interest.json):
-    python -m emod_api.config.from_overrides </path/to/po.json>
-
-To generate a default config.json based on the schema for a given Eradication binary:
-    python -m emod_api.config.from_schema -e </path/to/Eradication.[exe]> ...
-
-To generate a schema.json:
-    python -m emod_api.schema.get_schema </path/to/Eradication[.exe]>
-
-For rest of emod-api documentation, please go to https://github.com/InstituteforDiseaseModeling/emod-api
-"""
-__version__ = "1.34.0"
+__version__ = "1.35.0"
